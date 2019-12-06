0a46a1043bd1d7611ddc35ae880a24cf30c3dbb3
Michał Dziadowicz <michal@dziadowicz.it>
2019-12-06 19:04:25 +0100

    Refactor

--- redditdl.py
+++ redditdl.py
@@ -11,8 +11,8 @@ def download(subreddit):
     session = requests.Session()
     session.headers.update({'User-Agent': 'reddit-dl'})
     after = None
-    download_path = Path.cwd().joinpath('download', subreddit)
-    download_path.mkdir(parents=True, exist_ok=True)
+    posts_to_download = []
+
     while True:
         payload = {'after': after, 'limit': 100}
         result = session.get('http://www.reddit.com/r/' + subreddit + '/new.json', params=payload).json()
@@ -21,9 +21,7 @@ def download(subreddit):
         for post in result['data']['children']:
             if date.today() + timedelta(days=-2) < date.fromtimestamp(post['data']['created']) < date.today():
                 if 'post_hint' in post['data'] and post['data']['post_hint'] == 'image':
-                    result = session.get(post['data']['url'])
-                    if result.status_code == 200:
-                        download_image(post, download_path, result)
+                    posts_to_download.append(post)
                 else:
                     print(subreddit, post['data']['url'], datetime.fromtimestamp(post['data']['created']))
             elif date.fromtimestamp(post['data']['created']) < date.today() + timedelta(days=-2):
@@ -33,8 +31,17 @@ def download(subreddit):
                 break
         break
 
+    download_path = Path.cwd().joinpath('download', subreddit)
+    if posts_to_download:
+        download_path.mkdir(parents=True, exist_ok=True)
 
-def download_image(post, download_path, result):
+    for post in posts_to_download:
+        result = session.get(post['data']['url'])
+        if result.status_code == 200:
+            save_image(post, download_path, result)
+
+
+def save_image(post, download_path, result):
     permalink = post['data']['permalink'].split('/')
     filename = permalink[-2] + "#" + permalink[-3]
     fileext = "." + post['data']['url'].split('.')[-1].replace('?', '')
